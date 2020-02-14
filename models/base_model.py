46ec7bbee6a11c1b801957da3c622b09303bd103
Felipe Satizabal <1165@holbertonschool.com>
2020-02-14 16:53:48 -0500

    add folder models

---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+defines all common attributes/methods for other classes
+"""
+import datetime
+import models
+import uuid
+
+
+class BaseModel:
+    """
+    defines all common attributes/methods for other classes
+    """
+
+    def __init__(self, *args, **kwargs):
+        """constructor"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        datefmt = '%Y-%m-%dT%H:%M:%S.%f'
+                        value = datetime.datetime.strptime(value, datefmt)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """str method"""
+        msg = "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                    self.__dict__)
+        return msg
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+        self.updated_at = datetime.datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values"""
+        new_dict = self.__dict__
+        new_dict['__class__'] = self.__class__.__name__
+        cr_at = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        up_at = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        new_dict['created_at'] = cr_at
+        new_dict['updated_at'] = up_at
+        return new_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+serializes instances to JSON and deserializes JSON to instances
+"""
+import json
+from models.base_model import BaseModel
+
+
+
+class FileStorage:
+    """
+    serializes instances to JSON and deserializes JSON to instances
+    """
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def __init__(self):
+        """constructor"""
+
+    def all(self):
+        """returns the dictionary"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        name = obj.__class__.__name__
+        noid = obj.id
+        key = "{}.{}".format(name, noid)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        data = self.__objects.copy()
+        for key, value in self.__objects.items():
+            data[key] = value.to_dict()
+
+        namefile = FileStorage.__file_path
+        with open(namefile, mode="w", encoding="utf-8") as file:
+            json.dump(data, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        namefile = self.__file_path
+        try:
+            with open(namefile, encoding="utf-8") as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    nameClass = value["__class__"]
+                    del value["__class__"]
+                    newobj = eval(nameClass)(**value)
+                    FileStorage.__objects[key] = newobj
+        except Exception:
+            pass
