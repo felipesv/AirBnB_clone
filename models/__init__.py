c274b01cc60a79d48ed308fa910d8882b6e867cd
Felipe Satizabal <1165@holbertonschool.com>
2020-02-18 10:28:34 -0500

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''__init__ file'''
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Tests for FileStorage class'''
+import os
+import unittest
+
+class TestFile_Storage(unittest.TestCase):
+    '''start tests'''
+    
+    def test_docstring(self):
+        '''test'''
+        self.assertTrue(True)
