9c83edce4d16ea2581cad9fe142601c60b13a988
Felipe Satizabal <1165@holbertonschool.com>
2020-02-19 17:32:48 -0500

    fix

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python3
-'''Tests for BaseModel class'''
+'''Tests for State class'''
 import models
 import os
 import os.path
 import unittest
-from models.base_model import BaseModel
+from models.state import State
 from models.engine import file_storage
 from models.engine.file_storage import FileStorage
 
 
-class TestBase_Model(unittest.TestCase):
+class TestState(unittest.TestCase):
     '''start tests'''
 
     def setUp(self):
         '''Object created from a class'''
-        self.my_object = BaseModel()
+        self.my_object = State()
 
     def test_docstring(self):
         '''test if funcions, methods, classes
         and modules have docstring'''
         msj = "Módulo does not has docstring"
-        self.assertIsNotNone(models.base_model.__doc__, msj)  # Modules
+        self.assertIsNotNone(models.state.__doc__, msj)  # Modules
         msj = "Clase does not has docstring"
-        self.assertIsNotNone(BaseModel.__doc__, msj)  # Classes
+        self.assertIsNotNone(State.__doc__, msj)  # Classes
 
     def test_executable_file(self):
         '''test if file has permissions u+x to execute'''
         # Check for read access
-        is_read_true = os.access('models/base_model.py', os.R_OK)
+        is_read_true = os.access('models/state.py', os.R_OK)
         self.assertTrue(is_read_true)
         # Check for write access
-        is_write_true = os.access('models/base_model.py', os.W_OK)
+        is_write_true = os.access('models/state.py', os.W_OK)
         self.assertTrue(is_write_true)
         # Check for execution access
-        is_exec_true = os.access('models/base_model.py', os.X_OK)
+        is_exec_true = os.access('models/state.py', os.X_OK)
         self.assertTrue(is_exec_true)
 
     def test_is_an_instance(self):
-        '''check if my_object is an instance of BaseModel'''
-        self.assertIsInstance(self.my_object, BaseModel)
+        '''check if my_object is an instance of State'''
+        self.assertIsInstance(self.my_object, State)
 
     def test_id(self):
         '''test if the id of two instances are different'''
-        instance1 = BaseModel()
-        instance2 = BaseModel()
+        instance1 = State()
+        instance2 = State()
         self.assertNotEqual(instance1.id, instance2.id)
 
     def test_str(self):
         '''check if the output of str is in the specified format'''
         _dict = self.my_object.__dict__
-        string1 = "[BaseModel] ({}) {}".format(self.my_object.id, _dict)
+        string1 = "[State] ({}) {}".format(self.my_object.id, _dict)
         string2 = str(self.my_object)
         self.assertEqual(string1, string2)
 
@@ -70,7 +70,7 @@ class TestBase_Model(unittest.TestCase):
         self.assertIsInstance(my_dict_model, dict)
         for key, value in my_dict_model.items():
             flag = 0
-            if my_dict_model['__class__'] == 'BaseModel':
+            if my_dict_model['__class__'] == 'State':
                 flag += 1
             self.assertTrue(flag == 1)
         for key, value in my_dict_model.items():
@@ -84,7 +84,7 @@ class TestBase_Model(unittest.TestCase):
         self.my_object.name = "Holberton"
         self.my_object.my_number = 89
         my_object_json = self.my_object.to_dict()
-        my_object_kwargs = BaseModel(**my_object_json)
+        my_object_kwargs = State(**my_object_json)
         self.assertNotEqual(my_object_kwargs, self.my_object)
 
     def test_des_and_serialization(self):
